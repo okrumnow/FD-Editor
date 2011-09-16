780863007a47be6b5f245e71f409d9531ed2ec8a
Olaf Krumnow <okrumnow@gmx.de>
2011-09-16 17:42:29 +0200

    check if the supertype is de.jebc.Process

--- de.jebc.editor/src/de/jebc/editor/model/utils/ModelProvider.java
+++ de.jebc.editor/src/de/jebc/editor/model/utils/ModelProvider.java
@@ -3,7 +3,12 @@ package de.jebc.editor.model.utils;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.Type;
 import org.eclipse.jdt.ui.IWorkingCopyManager;
 import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.ui.IEditorInput;
@@ -40,10 +45,45 @@ public class ModelProvider {
 	private void buildModel() throws CoreException {
 		ICompilationUnit wc = wcm.getWorkingCopy(input);
 		
-		IType[] types = wc.getAllTypes();
-				
-		model = new FlowDiagram("TEST");
-		model.setObject(new Process(types[0].getFullyQualifiedName()));
+		IType type = wc.getAllTypes()[0];
+		
+		CompilationUnit ast = parse(wc);
+		
+		FindSupertype visitor = new FindSupertype();
+		ast.accept(visitor);
+		Type supertype = visitor.getSupertype();
+		
+		ITypeBinding binding = supertype.resolveBinding();
+		
+		String name = binding.getBinaryName();
+
+		if (name.equals("de.jebc.Process")) {
+			model = new FlowDiagram("TEST");
+			model.setObject(new Process(type.getFullyQualifiedName()));
+		}
+	}
+	
+	private static CompilationUnit parse(ICompilationUnit unit) {
+		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(unit);
+		parser.setResolveBindings(true);
+		return (CompilationUnit) parser.createAST(null); // parse
 	}
 
+	private class FindSupertype extends ASTVisitor {
+		
+		private Type supertype;
+
+		@Override
+		public boolean visit(org.eclipse.jdt.core.dom.TypeDeclaration node) {
+			supertype = node.getSuperclassType();
+			return false;
+		}
+
+		public Type getSupertype() {
+			return supertype;
+		}
+
+	}
 }
