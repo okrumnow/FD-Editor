0a6f3f8be06367ff5589e75b2ac6979fe9f1d202
Olaf Krumnow <okrumnow@gmx.de>
2011-09-18 14:12:36 +0200

    find additional In- and OutPin's and add to process

--- de.jebc.editor/src/de/jebc/editor/model/utils/ModelProvider.java
+++ de.jebc.editor/src/de/jebc/editor/model/utils/ModelProvider.java
@@ -1,20 +1,30 @@
 package de.jebc.editor.model.utils;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTParser;
 import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.ITypeBinding;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jdt.core.dom.Type;
+import org.eclipse.jdt.core.dom.TypeDeclaration;
 import org.eclipse.jdt.ui.IWorkingCopyManager;
 import org.eclipse.jdt.ui.JavaUI;
 import org.eclipse.ui.IEditorInput;
 
 import de.jebc.editor.model.FlowDiagram;
+import de.jebc.editor.model.InPin;
 import de.jebc.editor.model.ModelObject;
+import de.jebc.editor.model.OutPin;
 import de.jebc.editor.model.Process;
 
 public class ModelProvider {
@@ -44,25 +54,41 @@ public class ModelProvider {
 
 	private void buildModel() throws CoreException {
 		ICompilationUnit wc = wcm.getWorkingCopy(input);
-		
+
 		IType type = wc.getAllTypes()[0];
-		
+
 		CompilationUnit ast = parse(wc);
-		
+
 		FindSupertype visitor = new FindSupertype();
 		ast.accept(visitor);
 		Type supertype = visitor.getSupertype();
-		
+
 		ITypeBinding binding = supertype.resolveBinding();
-		
+
 		String name = binding.getBinaryName();
 
 		if (name.equals("de.jebc.Process")) {
 			model = new FlowDiagram("TEST");
-			model.setObject(new Process(type.getFullyQualifiedName()));
+			Process process;
+			process = new Process(type.getFullyQualifiedName());
+			process.addInPin(new InPin("Start"));
+			process.addOutPin(new OutPin("Result"));
+			model.setObject(process);
+
+			FindPins pinVisitor = new FindPins();
+			ast.accept(pinVisitor);
+			Map<MethodDeclaration, String> pins = pinVisitor.getPins();
+
+			for (MethodDeclaration method : pins.keySet()) {
+				String typeName = pins.get(method);
+				if (typeName.contains("InPin")) {
+					process.addInPin(new InPin(method.getName().toString()));
+				} else
+					process.addOutPin(new OutPin(method.getName().toString()));
+			}
 		}
 	}
-	
+
 	private static CompilationUnit parse(ICompilationUnit unit) {
 		ASTParser parser = ASTParser.newParser(AST.JLS3);
 		parser.setKind(ASTParser.K_COMPILATION_UNIT);
@@ -72,11 +98,11 @@ public class ModelProvider {
 	}
 
 	private class FindSupertype extends ASTVisitor {
-		
+
 		private Type supertype;
 
 		@Override
-		public boolean visit(org.eclipse.jdt.core.dom.TypeDeclaration node) {
+		public boolean visit(TypeDeclaration node) {
 			supertype = node.getSuperclassType();
 			return false;
 		}
@@ -84,6 +110,23 @@ public class ModelProvider {
 		public Type getSupertype() {
 			return supertype;
 		}
+	}
 
+	private class FindPins extends ASTVisitor {
+
+		private Map<MethodDeclaration, String> pinNodes = new HashMap<MethodDeclaration, String>();
+
+		public boolean visit(MethodDeclaration node) {
+			Type returnType = node.getReturnType2();
+			ITypeBinding binding = returnType.resolveBinding();
+			String name = binding.getBinaryName();
+			if (name.equals("de.jebc.InPin") || name.equals("de.jebc.OutPin"))
+				pinNodes.put(node, name);
+			return false;
+		}
+
+		public Map<MethodDeclaration, String> getPins() {
+			return pinNodes;
+		}
 	}
 }
